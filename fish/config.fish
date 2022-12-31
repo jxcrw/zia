adbd35974adfb2465b0e9ad891e5bb0e2856c32f
Jak Crow <jak@jxcrw.io>
2022-12-31 02:53:48 -0800

    +fish: Start customizing colors and keys

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,17 @@
 #!/usr/bin/env fish
 # Jak Crow 2022-∞ 💗
 
+set -g fish_term24bit 1
 set -U fish_greeting
 set -U EDITOR nano
 set -U WINHOME 'C:/~'
 
+set -U fish_color_selection --background='304B4F'
+
 
 # General
 alias home='cd ~'
+alias sel='cd ~/.sel'
 alias u='cd ..'
 alias ls='LC_COLLATE=C command ls -NA --color --group-directories-first'
 alias copy='cp'
@@ -73,7 +77,25 @@ function fish_right_prompt
     echo $git_info
 end
 
+function space_prompts --on-event fish_postexec
+   echo
+end
+
 set -U LS_COLOR 'rs=0:di=01;34:ln=01;3:mh=00:fi=40;33:pi=40;33:so=01;35:do=01;35:bd=40;33;cd=40;33;or=40;31;mi=00:su=37;41:sg=30;43:ca=30;41:tw=30:ow=34:st=37;44:ex=01;37'
+
+
+# printf \e\[H\e\[2J
+
+function print_pizza
+    echo "pizza"
+end
+
+function fish_user_key_bindings
+    bind \e\[3\;8~ print_pizza
+    bind \b backward-kill-word
+    bind \cU kill-whole-line
+    bind \cY redo
+end
 
 
 function profile
@@ -84,3 +106,65 @@ end
 
 
 zoxide init --cmd a fish | source
+
+
+# Tweak according to need. This is not a complete list of keys!
+set --local letters 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
+set --local numbers '1234567890'
+set --local special '!@#$%^&*_+-=\\;,./<>?:|'
+set --local pairs '\'"()[]{}'
+set --local characters (string split '' "$letters$numbers$special$pairs")
+
+# Use fish_key_reader to find out bindings. These are a combination of escape sequences and hex codes.
+# The following should already be sent by your terminal:
+set --local left                \e'[C'
+set --local right               \e'[D'
+set --local shift_left          \e'[1;2D'
+set --local shift_right         \e'[1;2C'
+set --local backspace           \x7f
+set --local delete              \e'[3~'
+set --local option_left         \eb
+set --local option_right        \ef
+# The following will need to be added to be sent by your terminal:
+set --local option_shift_left   \e'[1;6D'
+set --local option_shift_right  \e'[1;6C'
+set --local command_shift_left  \e\[1\;2H
+set --local command_shift_right \e\[1\;2F
+set --local command_left        \ca
+set --local command_right       \ce
+set --local command_backspace   \cU
+set --local command_delete      \ck
+set --local option_backspace    \cw
+set --local option_delete       \ed
+set --local command_c           \cC
+set --local command_x           \cX
+set --local command_v           \cV
+
+if functions --query _natural_selection
+  bind $left                '_natural_selection forward-char'
+  bind $right               '_natural_selection backward-char'
+  bind $shift_left          '_natural_selection backward-char --is-selecting'
+  bind $shift_right         '_natural_selection forward-char --is-selecting'
+  bind $command_left        '_natural_selection beginning-of-line'
+  bind $command_right       '_natural_selection end-of-line'
+  bind $command_shift_left  '_natural_selection beginning-of-line --is-selecting'
+  bind $command_shift_right '_natural_selection end-of-line --is-selecting'
+  bind $option_left         '_natural_selection backward-word'
+  bind $option_right        '_natural_selection forward-word'
+  bind $option_shift_left   '_natural_selection backward-word --is-selecting'
+  bind $option_shift_right  '_natural_selection forward-word --is-selecting'
+  bind $delete              '_natural_selection delete-char'
+  bind $backspace           '_natural_selection backward-delete-char'
+  bind $command_delete      '_natural_selection kill-line'
+  bind $command_backspace   '_natural_selection backward-kill-line'
+  bind $option_backspace    '_natural_selection backward-kill-word'
+  bind $option_delete       '_natural_selection kill-word'
+  bind $command_c           'fish_clipboard_copy'
+  bind $command_x           '_natural_selection cut-to-clipboard'
+  bind $command_v           'fish_clipboard_paste'
+
+  for character in $characters
+    set --local escaped (string escape -- $character)
+    bind $character "_natural_selection --is-character -- $escaped"
+  end
+end
